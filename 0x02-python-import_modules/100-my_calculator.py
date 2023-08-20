f1ccee5df147d42ac2e0fe2a81d44710debb4856
Lord-Bish <uchennabobbyfrade@yahoo.com>
2023-08-20 04:13:07 -0700

    corrected file

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    
+
     signs = ["+", "-", "*", "/"]
     num = len(sys.argv) - 1
 
     if num != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
+
     operator = sys.argv[2]
 
     if operator not in signs:
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         sys.exit(1)
 
     from calculator_1 import add, sub, mul, div
-    
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
