785e4d41d686a22c19f2c9e975cd9b346a33397c
Lord-Bish <uchennabobbyfrade@yahoo.com>
2023-09-05 03:45:16 -0700

    corrections

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,11 +46,9 @@ class Square:
             return
 
         for i in range(0, self.__position[1]):
-            print("--")
+            print()
 
         for i in range(0, self.__size):
             for j in range(self.__position[0]):
-                print(" ", end="")
-            for k in range(0, self.__size):
-                print("#", end="")
+                print("#" * self.__size, end="")
             print("")
