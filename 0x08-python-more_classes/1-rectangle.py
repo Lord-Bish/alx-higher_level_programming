423b7bd14ce8944c1cf4efafeed109ffb7993faf
Lord-Bish <uchennabobbyfrade@yahoo.com>
2023-09-07 04:56:13 -0700

    more classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -3,6 +3,7 @@
 class Rectangle:
     """ a rectangle class """
     def __init__(self, width=0, height=0):
+        """ initializes class instances """
         self.width = width
         self.height = height
 
