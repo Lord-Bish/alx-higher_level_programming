7c956f94337cc2cb43f02f548d3d2bc0715819e0
Lord-Bish <uchennabobbyfrade@yahoo.com>
2023-09-10 23:03:23 -0700

    correction

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-""" This module creates a class rectangle """
+"""This module creates a class rectangle"""
 class Rectangle:
-    """ a rectangle class """
+    """a rectangle class"""
     def __init__(self, width=0, height=0):
-        """ initializes class instances """
+        """
+        Constructor of the class Rectangle
+          Args:
+            - width (default = 0): int
+            - heigth (default = 0): int
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """ a getter method """
+        """a getter method"""
         return (self.__width)
 
     @width_setter
     def width(self, value):
-        """ a setter method """
+        """
+        a setter method of with attribute
+          Args:
+            - value: int
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -24,12 +33,16 @@ class Rectangle:
 
     @property
     def height(self):
-        """ a getter method """
+        """a getter method"""
         return (self.__height)
 
     @height_setter
     def height(self, value):
-        """ a setter method """
+        """
+        a setter of the height attribute
+          Args:
+            - value: int
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         elif height < 0:
