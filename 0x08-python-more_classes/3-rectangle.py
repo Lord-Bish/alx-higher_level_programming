3fcfa97ab325fef8ead9cc8b05e679afcf4f41a5
Lord-Bish <uchennabobbyfrade@yahoo.com>
2023-09-10 23:45:04 -0700

    correction

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
         """ a getter method """
         return (self.__width)
 
-    @width_setter
+    @width.setter
     def width(self, value):
         """a setter method"""
         if not isinstance(value, int):
@@ -36,7 +36,7 @@ class Rectangle:
         """ a getter method """
         return (self.__height)
 
-    @height_setter
+    @height.setter
     def height(self, value):
         """ a setter method """
         if type(value) is not int:
@@ -56,5 +56,3 @@ class Rectangle:
             return (0)
         else:
             return (2 * (self.__width + self.__height))
-
-
