810d648ca9484e06a55ca2edc0cdbcfc850efe1f
Lord-Bish <uchennabobbyfrade@yahoo.com>
2023-09-23 09:23:48 -0700

    corrections

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """class constructor"""
 
-        super.__init__(size, size, x, y, id,)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         """a string representation"""
